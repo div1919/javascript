a72aeecfdb42af22c7cd0a8a3cfe3afe681ac8a8
div1919 <116082564+div1919@users.noreply.github.com>
2024-08-09 22:17:44 +0000

    js-variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 191919
+let accountEmail = "div@gmail.com"
+var accountPassword = "919191"
+accountCity = "Delhi"
+let accountState;
+
+accountEmail = "div19@gmail.com"
+accountPassword = "12345"
+accountCity = "Noida"
+
+// accountId = 4 //constant can't be changed
+console.log(accountId);
+
+/*
+Prefer not to use var
+bacoz of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
