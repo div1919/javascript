0377be5c3ae619868c8d9415e0a62fac73f6b31f
div1919 <116082564+div1919@users.noreply.github.com>
2024-08-10 21:03:41 +0000

    operation

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,31 @@
+let value = 3
+let negvalue = -value
+
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2 to the power 3
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Div!"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // if string is first then all are treated as string
+// console.log(1 + 2 + "2"); 
+
+// console.log(+true); //coz of + it converts to number i.e. 1
+// console.log(+""); //as empty string is false so it converts to 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 //not a good practice
+console.log(num1);
