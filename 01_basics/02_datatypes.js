1b4906c1edc11cd48ae070ccb2bd8fe6d7047f71
div1919 <116082564+div1919@users.noreply.github.com>
2024-08-10 20:07:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+let name = "div"
+let age = 22
+let isLoggedIn = false
+let state;
+
+
+// number => numbers(2,3,4)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
