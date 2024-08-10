c9a8d1331e6a1619dbed71a3cca08916abeeee39
div1919 <116082564+div1919@users.noreply.github.com>
2024-08-10 21:56:32 +0000

    memory type

---
+++ 01_basics/05_Memory.js
@@ -0,0 +1,22 @@
+// Stack(used in Primitive type) and Heap(used in Non-primitive type) Memory
+
+let myName = "Div!!"
+
+let anotherName = myName
+anotherName = "Sonu"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "322@ybl",
+
+}
+
+let userTwo = userOne
+userTwo.email = "user1@email.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
