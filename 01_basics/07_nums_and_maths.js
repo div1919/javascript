bd24ee3cb5cd11ea78dfe649363050355f88031d
div1919 <116082564+div1919@users.noreply.github.com>
2024-08-14 19:43:44 +0000

    nums and maths

--- 01_basics/07_nums_and_maths.js
+++ 01_basics/07_nums_and_maths.js
@@ -1,17 +1,40 @@
-const score = 500
-console.log(score);
+// const score = 500
+// console.log(score);
 
-const balance = new Number(1000)
-console.log(balance);
+// const balance = new Number(1000)
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2)); //fixed -> give the 2(n) decimal value
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //fixed -> give the 2(n) decimal value
 
-const otherNumber = 119.915191
-console.log(otherNumber.toPrecision(4)); //use precision after 4(n) numbers
+// const otherNumber = 119.915191
+// console.log(otherNumber.toPrecision(4)); //use precision after 4(n) numbers
 
-const hundreds = 1000000
-console.log(hundreds.toLocaleString()); //use comma in big numbers according to US system
-console.log(hundreds.toLocaleString('en-IN')); //Indian system
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString()); //use comma in big numbers according to US system
+// console.log(hundreds.toLocaleString('en-IN')); //Indian system
+
+// ************* Maths ****************************
+
+console.log(Math);
+console.log(Math.abs(-4)); //gives absolute value of number
+console.log(Math.round(4.6)); //round of the number 
+console.log(Math.ceil(4.2)); //round off to upper value always
+console.log(Math.floor(4.9)); //round off to lower value always
+console.log(Math.min(4, 3, 2, 1, 5)); //find min in an array
+console.log(Math.max(4, 3, 2, 5, 2)); //find max in an array
+
+console.log(Math.random()); //random -> always return some value between 0 & 1
+console.log((Math.random() * 10) + 1);
+ 
+console.log(Math.floor((Math.random()*10) + 1)); //
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min)) + min); //it gives value always greater than min and less than max
+
+
+
 
 
