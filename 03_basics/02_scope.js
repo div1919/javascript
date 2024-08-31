2223306dda1d1e2c79fc138d21edae5a2dd237c3
div1919 <116082564+div1919@users.noreply.github.com>
2024-08-31 09:43:34 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,15 @@
+// var c = 300
+let a = 100
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("In the condition:",a);    
+}
+
+
+
+console.log("outside the condition:",a);
+// console.log(b);
+// console.log(c); // var is not good coz it doesn't get effected by scope
