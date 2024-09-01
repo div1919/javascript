cd380798fa8d29e65e5c02e043651ec48bca08db
div1919 <116082564+div1919@users.noreply.github.com>
2024-09-01 21:09:26 +0000

    Scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -8,8 +8,45 @@ if(true){
     console.log("In the condition:",a);    
 }
 
-
-
-console.log("outside the condition:",a);
+// console.log("outside the condition:",a);
 // console.log(b);
 // console.log(c); // var is not good coz it doesn't get effected by scope
+
+function one(){
+    const username = "Divyanshu"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website); // gives error
+    two()    
+}
+
+one()
+
+if(true){
+    const username = "Div"
+    if(username == "Div"){
+        const website = " reddit"
+        console.log(username + website);        
+    }
+    // console.log(website);    //gives error
+}
+
+// console.log(username); // gives error
+
+// +++++++++++++++++++++++++ interesting ++++++++++++++++++
+
+function addOne(num){
+    return num + 1
+}
+
+console.log(addOne(5));
+
+// addTwo(5) // gives error => not accessible before initialization
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addTwo(5));
